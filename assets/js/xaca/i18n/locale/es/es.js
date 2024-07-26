11e0cab1b4ef331d71bc24a362219038c24ac379
Juan José Jaramillo <74982329+juan3312@users.noreply.github.com>
2024-07-25 21:50:32 -0500

    Se agrega palabras en traductor

--- assets/js/xaca/i18n/locale/es/es.js
+++ assets/js/xaca/i18n/locale/es/es.js
@@ -3,6 +3,28 @@ export default {
   "tp_menu_2":"NOSOTROS",
   "tp_menu_3":"SERVICIOS",
   "tp_menu_4":"CONTÁCTANOS",
+  "tp-bn-h4-1":"SOLUCIONES PERSONALIZADAS",
+  "tp-bn-h4-2":"IMPULSAMOS TU CRECIMIENTO",
+  "tp-bn-h4-3":"UN EQUIPO ALTAMENTE CALIFICADO",
+  "tp-bn-p-1":"Que impulsan la eficiencia, productividad <br> y creciminento de tu empresa.",
+  "tp-bn-p-2":"Creamos valor a través de nuestras tres  líneas de servicios.",
+  "tp-bn-p-3":"Tus proyectos se completarán de manera oportuna y con los más altos estándares de calidad.",
+  "tp-btn":"VER MÁS",
+  "tp-form-1":"¡Solicita una llamada ahora mismo!",
+  "tp-txt-form":"Con gusto te atenderemos",
+  "tp-service-hd-title":"Nuestros Servicios",
+  "tp-service-hd-span":"Impulsamos tu crecimineto y creamos valor a tráves de nuestras tres lineas de servicio",
+  "tp-service-hd-title-1":"Consultoría Y Gestión",
+  "tp-service-hd-title-2":"Outsourcing",
+  "tp-service-hd-title-3":"Staffing",
+  "tp-hd-more-info-title":"Quiénes somos",
+  "tp-hd-more-info-p":"Tras años liderando procesos ejecutivos, estratégicos y financieros en múltiples organizaciones de diversos sectores, aprendimos a identificar y abordar las necesidades reales a las que se enfrentan las empresas para acelerar su crecimiento. Fundamos 3 Pinos para compartir el conocimiento y las lecciones aprendidas con empresas en crecimiento que necesitan avanzar al siguiente nivel.",
+  "tp-conf-title":"Confían en nosotros",
+  "tp-callback-title":"Solicitar una llamada",
+  "tp-callback-p":"Con gusto nos pondremos en contacto contigo",
+  "tp-footer-contact-title":"Contact",
+  "tp-footer-contact-p":"Weston – Florida United States  Medellín - Antioquia  Colombia",
+
 };
 
 /*
